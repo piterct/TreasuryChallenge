3140eb37543b4a0e91ca23013a4fcc13f37bd321
Michael Peter <michael_piterct@hotmail.com>
2021-10-16 19:23:47 -0300

    text File entity

---
+++ TreasuryChallenge/TreasuryChallenge.Domain/Entity/TextFile.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using TreasuryChallenge.Domain.Utils;
+
+namespace TreasuryChallenge.Domain.Entity
+{
+    public class TextFile
+    {
+        public string FileName { get; set; }
+        public int MaxLengthContent { get; set; }
+        public TextFile(string fileName, int maxLengthContent)
+        {
+            this.FileName = fileName;
+            this.MaxLengthContent = maxLengthContent;
+        }
+
+        public void WriteFile(int inputValue)
+        {
+            string fileName = $@"{this.FileName}.txt";
+
+            using (StreamWriter write = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < inputValue; i++)
+                {
+                    write.WriteLine(GenerateContent(string.Empty));
+                };
+            };
+        }
+
+        public string GenerateContent(string updatedAlphabetLetters = "", string content = "")
+        {
+            string alphabetLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (content.Length > 0)
+                alphabetLetters = updatedAlphabetLetters;
+
+            if (content.Length == this.MaxLengthContent) return content;
+
+            string charGenerated = TreasuryUtil.GetChar(alphabetLetters);
+
+            if (content.Length != 0)
+            {
+                bool found = TreasuryUtil.FoundChar(content, charGenerated);
+                while (found)
+                {
+                    alphabetLetters = alphabetLetters.Replace(charGenerated, "");
+                    charGenerated = TreasuryUtil.GetChar(alphabetLetters);
+                    found = TreasuryUtil.FoundChar(content, charGenerated);
+                }
+            }
+            return GenerateContent(alphabetLetters, content + charGenerated);
+        }
+    }
+}
---
+++ TreasuryChallenge/TreasuryChallenge.Domain/Utils/TreasuryUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TreasuryChallenge.Domain.Utils
+{
+    public static class TreasuryUtil
+    {
+        public static string GetChar(string alhabehticLetters)
+        {
+            Random random = new Random();
+            char[] chars = alhabehticLetters.ToCharArray(0, alhabehticLetters.Length);
+
+            return chars[random.Next(alhabehticLetters.Length - 1)].ToString();
+        }
+        public static bool FoundChar(string content, string charGenerated)
+        {
+            return (content.ToUpper().Contains(charGenerated.ToUpper()));
+        }
+    }
+}
