7cfd7eb9143cde8613535f33b60e5f1b19c492ee
Michael Peter <michael_piterct@hotmail.com>
2021-10-16 19:05:16 -0300

    Shared and Command Input

---
+++ TreasuryChallenge/TreasuryChallenge.Domain/Commands/Inputs/GetLinesAmountToWriteCommandInput.cs
@@ -0,0 +1,20 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using TreasuryChallenge.Shared.Commands.Contracts;
+
+namespace TreasuryChallenge.Domain.Commands.Inputs
+{
+    public class GetLinesAmountToWriteCommandInput : Notifiable, ICommand
+    {
+        public int LinesAmount { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+           new Contract()
+               .Requires()
+               .IsNotNull(LinesAmount, "LinesAmount", "This value is not valid!")
+                );
+        }
+    }
+}
---
+++ TreasuryChallenge/TreasuryChallenge.Shared/Commands/Contracts/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using Flunt.Validations;
+
+namespace TreasuryChallenge.Shared.Commands.Contracts
+{
+    public interface ICommand : IValidatable
+    {
+    }
+}
