b01e495c87e08a02c25b77ec1248f012ead81a20
Michael Peter <michael_piterct@hotmail.com>
2021-10-17 14:00:54 -0300

    foundchar tests

---
+++ TreasuryChallenge/TreasuryChallenge.Tests/Utils/TreasuryUtilTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreasuryChallenge.Domain.Utils;
+
+namespace TreasuryChallenge.Tests.Utils
+{
+    [TestClass]
+    public class TreasuryUtilTests
+    {
+        [TestMethod]
+        public void Valid_OneCharInsideOfString_ReturnTrue()
+        {
+            string content = "UJHSNKL";
+            bool result = TreasuryUtil.FoundChar(content, "U");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
