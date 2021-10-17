a97389661fe0dede1eaab28fee72763b359c0afa
Michael Peter <michael_piterct@hotmail.com>
2021-10-17 13:57:45 -0300

    teste Generete_Char_ReturnTotalCharEqualThePastValue

--- TreasuryChallenge/TreasuryChallenge.Tests/Entities/TextFileTests.cs
+++ TreasuryChallenge/TreasuryChallenge.Tests/Entities/TextFileTests.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreasuryChallenge.Domain.Entities;
 
 namespace TreasuryChallenge.Tests.Entities
 {
     [TestClass]
     public class TextFileTests
     {
+        [TestMethod]
+        public void Generete_Char_ReturnTotalCharEqualThePastValue()
+        {
+            TextFile txtFile = new TextFile("file-teste", 8);
+            string content = txtFile.GenerateContent().Result;
+
+            Assert.AreEqual(8, content.Length);
+        }
     }
 }
