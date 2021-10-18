85d6b009a388539ab348569df283afd7155746c4
Michael Peter <michael_piterct@hotmail.com>
2021-10-18 00:10:56 -0300

    commands to tests

---
+++ TreasuryChallenge/TreasuryChallenge.Tests/Commands/GetLinesAmountToWriteCommandInputTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreasuryChallenge.Domain.Commands.Inputs;
+
+namespace TreasuryChallenge.Tests.Commands
+{
+    [TestClass]
+    public class GetLinesAmountToWriteCommandInputTests
+    {
+        [TestMethod]
+        [TestCategory("Commands")]
+
+        public void Valid_CommandAmountToWriteLines_ReturnTrue()
+        {
+            GetLinesAmountToWriteCommandInput command = new GetLinesAmountToWriteCommandInput(5);
+            command.Validate();
+
+            Assert.IsTrue(command.Valid);
+        }
+
+        [TestMethod]
+        [TestCategory("Commands")]
+        public void Valid_CommandAmountToWriteLines_Returnfalse_Value_Zero()
+        {
+            GetLinesAmountToWriteCommandInput command = new GetLinesAmountToWriteCommandInput(0);
+            command.Validate();
+
+            Assert.IsTrue(command.Invalid);
+        }
+
+        [TestMethod]
+        [TestCategory("Commands")]
+        public void Valid_CommandAmountToWriteLines_Returnfalse_Value_Empty()
+        {
+            GetLinesAmountToWriteCommandInput command = new GetLinesAmountToWriteCommandInput();
+            command.Validate();
+
+            Assert.IsTrue(command.Invalid);
+        }
+    }
+}
