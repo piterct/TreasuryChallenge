32e5368fc81fa30212028e09f92b75e168433397
Michael Peter <michael_piterct@hotmail.com>
2021-10-18 00:12:05 -0300

    treasury handler tests

--- TreasuryChallenge/TreasuryChallenge.Tests/Handlers/TreasuryHandlerTests.cs
+++ TreasuryChallenge/TreasuryChallenge.Tests/Handlers/TreasuryHandlerTests.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TreasuryChallenge.Domain.Commands.Inputs;
 using TreasuryChallenge.Domain.Commands.Result;
 using TreasuryChallenge.Domain.Handlers;
-using TreasuryChallenge.Domain.Repositories;
+using TreasuryChallenge.Shared.Settings;
 using TreasuryChallenge.Tests.Fakes;
 
 namespace TreasuryChallenge.Tests.Handlers
@@ -14,16 +15,19 @@ namespace TreasuryChallenge.Tests.Handlers
         private readonly TreasuryHandler _treasuryHandler;
         private readonly GetLinesAmountToWriteCommandInput _getLinesAmountToWriteCommandInput;
         private readonly GetLinesAmountToWriteCommandInput _getLinesAmountToWriteCommandInputZeroLine;
+        private IOptions<TreasurySettings> _optionsTreasurySettings;
 
         public TreasuryHandlerTests()
         {
+            _optionsTreasurySettings = Options.Create(new TreasurySettings() { NameFile = "aleatory-file", MaxLengthCode = 7, Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" });
             _fakeTextFileRepository = new FakeTextFileRepository();
-            _treasuryHandler = new TreasuryHandler(_fakeTextFileRepository);
+            _treasuryHandler = new TreasuryHandler(_optionsTreasurySettings, _fakeTextFileRepository);
             _getLinesAmountToWriteCommandInput = new GetLinesAmountToWriteCommandInput(10);
             _getLinesAmountToWriteCommandInputZeroLine = new GetLinesAmountToWriteCommandInput(0);
         }
 
         [TestMethod]
+        [TestCategory("Handlers")]
         public void Call_WriteCodeInFile_MustExecuteSuccessfully()
         {
             GetLinesAmountToWriteCommandResult handler = _treasuryHandler.Handle(_getLinesAmountToWriteCommandInput).Result;
@@ -32,6 +36,7 @@ namespace TreasuryChallenge.Tests.Handlers
         }
 
         [TestMethod]
+        [TestCategory("Handlers")]
         public void Call_WriteCodeInFile_MustExecuteFail()
         {
             GetLinesAmountToWriteCommandResult handler = _treasuryHandler.Handle(_getLinesAmountToWriteCommandInputZeroLine).Result;
