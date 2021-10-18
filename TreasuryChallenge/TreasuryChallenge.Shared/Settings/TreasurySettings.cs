8f97a7b55e7b6aed585c8a2a393f80e711b4e02a
Michael Peter <michael_piterct@hotmail.com>
2021-10-18 00:10:24 -0300

    shared configurations

---
+++ TreasuryChallenge/TreasuryChallenge.Shared/Settings/TreasurySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TreasuryChallenge.Shared.Settings
+{
+    public class TreasurySettings
+    {
+        public string NameFile { get; set; }
+        public int MaxLengthCode { get; set; }
+        public string Characters { get; set; }
+    }
+}
